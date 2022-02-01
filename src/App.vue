f4528ec41952dc2e13629f09461cb5413a36caa3
StNixe <stnixe.adm@gmail.com>
2022-02-01 03:22:14 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <span>...app...</span>
+    <img src="./assets/img/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+html,
+body {
+  margin: 0px;
+  padding: 0px;
+  font-family: sans-serif;
+  background-color: #22222a;
+  color: #fefefe;
+}
+#app {
+  background-color: tomato;
+  color: #fefefe;
+}
+</style>
